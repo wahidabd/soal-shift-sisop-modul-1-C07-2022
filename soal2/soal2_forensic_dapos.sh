e85f54e567d10355894669b98fdf4032516eb0b2
root <root@Legion.localdomain>
2022-02-23 22:26:46 +0700

    add soal2

--- soal2/soal2_forensic_dapos.sh
+++ soal2/soal2_forensic_dapos.sh
@@ -4,15 +4,43 @@ locLog=$locFolder/log_website_daffainfo.log
 locRata=$locFolder/ratarata.txt
 locResult=$locFolder/result.txt
 
-#b
-#awk -F '"' '{ sum += $4 }
-#	END {print sum}' $locLog
+#B
+cat $locLog | awk '{split($0,a,":")
+	b[a[2]":"a[3]"\""]++}
+		END{
+			for(i in b) {
+		 		count++
+			 	summ+=b[i]
+			}
+			summ=summ/count
+			echo "rata rata serangan perjam adalah sebanyak %.3f request per jam " $summ;
+		}' >> $locRata
 
 #C
-#awk -F '"' '{print $2}' $locLog
-awk -F '"' '{
-		count[$2]++
+cat $locLog | awk '{split($0,a,":")
+	b[a[1]]++}
+		END{
+			max = 0
+			target
+			for(i in b) {
+				if (max < b[i]){
+					target = i
+					max = b[target]
+				}
+			}
+			echo "yang paling banyak menagkses server adalah : " target " sebanyak " max " request\n"
+		}' >> $locResult
+
+#D
+cat $locLog  | awk '/curl/  { ++n }
+END   { print "ada", n, "request yang menggunakan curl sebagai user-agent\n" }' >> result.txt
+
+#E
+cat $locLog | awk '/22\/Jan\/2022:02/{
+	split($0,a,":")
+	b[a[1]]++
+	count++}END{
+		for (i in b){
+			echo i " IP Address Jam 2 Pagi"
 		}
-		END {
-			for ( i in count ) echo i, echo count[i]
-		}' $locLog
+	}' >> $locResult
