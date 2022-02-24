ab8ca370cfd9b7e8b81f4c2e8baccda0a32d3229
root <root@Legion.localdomain>
2022-02-24 11:08:49 +0700

    fix soal2

--- soal2/soal2_forensic_dapos.sh
+++ soal2/soal2_forensic_dapos.sh
@@ -5,42 +5,41 @@ locRata=$locFolder/ratarata.txt
 locResult=$locFolder/result.txt
 
 #B
-cat $locLog | awk '{split($0,a,":")
-	b[a[2]":"a[3]"\""]++}
-		END{
-			for(i in b) {
-		 		count++
-			 	summ+=b[i]
-			}
-			summ=summ/count
-			echo "rata rata serangan perjam adalah sebanyak %.3f request per jam " $summ;
-		}' >> $locRata
+cat $locLog | awk -F: '{gsub(/"/, "", $3)
+	arr[$3]++}
+	END {
+		for (i in arr) {
+			count++
+			res+=arr[i]
+		}
+		res/=count
+		printf "rata rata serangan perjam adalah sebanyak %.3f request per jam\n\n", res
+	}' >> $locRata
 
 #C
-cat $locLog | awk '{split($0,a,":")
-	b[a[1]]++}
-		END{
-			max = 0
-			target
-			for(i in b) {
-				if (max < b[i]){
-					target = i
-					max = b[target]
-				}
+cat $locLog | awk -F: '{gsub(/"/, "", $1)
+	arr[$1]++}
+	END {
+		max=0
+		target
+		for (i in arr){
+			if (max < arr[i]){
+				target = i
+				max = arr[target]
 			}
-			echo "yang paling banyak menagkses server adalah : " target " sebanyak " max " request\n"
-		}' >> $locResult
+		}
+		print "yang paling banyak mengakses server adalah: " target " sebanyak " max " request\n"
+	}' >> $locResult
 
 #D
-cat $locLog  | awk '/curl/  { ++n }
-END   { print "ada", n, "request yang menggunakan curl sebagai user-agent\n" }' >> result.txt
+cat $locLog | awk '/curl/ {++n} END {
+print "ada " n " request yang menggunakan curl sebagai user-agent\n"}' >> $locResult
 
 #E
-cat $locLog | awk '/22\/Jan\/2022:02/{
-	split($0,a,":")
-	b[a[1]]++
-	count++}END{
-		for (i in b){
-			echo i " IP Address Jam 2 Pagi"
+cat $locLog | awk -F: '/2022:02/ {gsub(/"/, "", $1)
+	arr[$1]++}
+	END {
+		for (i in arr){
+			print i " Jam 2 Pagi"
 		}
 	}' >> $locResult
